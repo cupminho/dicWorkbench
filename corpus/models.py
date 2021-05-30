860fdb8bdef098d7abae637c18ae8917d3d6ced7
cupminho <cupminho@gmail.com>
2021-05-30 15:57:21 +0900

    추가

--- corpus/models.py
+++ corpus/models.py
@@ -5,6 +5,7 @@ class Document(models.Model):
     title = models.CharField(max_length=120, null=False)
     author = models.CharField(max_length=20, null=False)
     file = models.FileField(upload_to='text/')
+    status = models.IntegerField(default=0, auto_created=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
